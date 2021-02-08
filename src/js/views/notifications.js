75b6d51cafb3ddb16ea972d915ce2fcf9403e3bb
FIRST_NAME LAST_NAME <Maximvo92@gmail.com>
2021-02-08 15:26:15 +0300

    1

---
+++ src/js/views/notifications.js
@@ -0,0 +1,16 @@
+// alert-info - boostrap's class
+
+/**
+ * Function notify. Show notification message
+ * @param {Object} settings
+ * @param {string} settings.msg
+ * @param {string} settings.className
+ * @param {number} settings.msg
+ */
+export function notify({
+    msg = 'Info message',
+    className = 'alert-info',
+    timeout = 2000
+} = {}) {
+    
+}
